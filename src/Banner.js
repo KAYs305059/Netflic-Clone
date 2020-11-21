dec149ca3c5358f12b38fb6401d898882d4b6c5b
Khadar Ahmed Yusuf <Khadar_yo@hotmail.com>
2020-11-21 05:56:56 +0100

    adding youtube trailers

--- src/Banner.js
+++ src/Banner.js
@@ -51,7 +51,7 @@ function Banner() {
             {truncate(movie?.overview, 150)}
           </h1>
         </div>
-        <div className='banner--fadeBotto' />
+        <div className='banner--fadeBottom' />
       </header>
     </div>
   );
